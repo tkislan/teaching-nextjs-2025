7a5151f94dc8abafcd51a8c4b1c6728b585b658f
tomas <tomas@kislan.sk>
2025-09-30 08:10:23 +0000

    Add about and album details page

---
+++ src/app/album/[id]/page.tsx
@@ -0,0 +1,79 @@
+import { getDb } from "@/lib/db";
+
+function formatDuration(duration: number): string {
+  const minutes = Math.floor(duration / 60);
+  const seconds = duration % 60;
+
+  return `${minutes}` + ":" + `${seconds}`.padStart(2, "0");
+}
+
+export default async function AlbumDetail({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const db = getDb();
+
+  const { id } = await params;
+
+  console.log("Album detail id:", id);
+
+  const albumId = parseInt(id);
+
+  if (isNaN(albumId)) {
+    return <div>Invalid Album id</div>;
+  }
+
+  const album = await db
+    .selectFrom("albums")
+    .innerJoin("authors", "authors.id", "albums.author_id")
+    .select([
+      "albums.name",
+      "albums.release_date",
+      "authors.name as author_name",
+    ])
+    .where("albums.id", "=", albumId)
+    .executeTakeFirst();
+
+  // if (album == null)
+  if (album === null || album === undefined) {
+    // throw new Error("Not Found");
+    return <div>Album not found</div>;
+  }
+
+  const songs = await db
+    .selectFrom("songs")
+    .selectAll()
+    .where("album_id", "=", albumId)
+    .execute();
+
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div>
+          {album.name} by {album.author_name}
+        </div>
+        <div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Duration</th>
+              </tr>
+            </thead>
+            <tbody>
+              {songs.map((song, i) => (
+                <tr key={song.id}>
+                  <td>{i + 1}</td>
+                  <td>{song.name}</td>
+                  <td>{formatDuration(song.duration)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { DB } from "@/lib/db-types";
-import SQLite from "better-sqlite3";
-import { Kysely, SqliteDialect } from "kysely";
+import { getDb } from "@/lib/db";
+import Link from "next/link";
 
 export default async function Home() {
-  const dialect = new SqliteDialect({ database: new SQLite("db.sqlite") });
-  const db = new Kysely<DB>({ dialect });
+  const db = getDb();
 
   const albums = await db
     .selectFrom("albums")
@@ -21,18 +19,25 @@ export default async function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <p className="text-4xl font-bold">Spotify</p>
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-2 gap-4">
           {albums.map((album) => (
-            <div key={album.id} className="card w-96 bg-base-100 shadow-sm">
+            <div key={album.id} className="card w-64 bg-base-100 shadow-sm">
               <div className="card-body">
                 <span className="badge badge-xs badge-warning">Pop</span>
                 <h2 className="text-3xl font-bold">{album.name}</h2>
+
+                <p>ID: {album.id}</p>
                 <p>Author: {album.author_name}</p>
                 <p>
                   Release Date: {new Date(album.release_date).toDateString()}
                 </p>
                 <div className="mt-6">
-                  <button className="btn btn-primary btn-block">Details</button>
+                  <Link
+                    className="btn btn-primary btn-block"
+                    href={`/album/${album.id}`}
+                  >
+                    Detail
+                  </Link>
                 </div>
               </div>
             </div>
