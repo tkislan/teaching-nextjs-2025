8e47a31aab68d131abf3843670b4db1df5f9f5e9
tomas <tomas@kislan.sk>
2025-10-21 08:13:19 +0000

    Add simple NavBar

---
+++ src/app/NavBar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+export function NavBar() {
+  const [searchInput, setSearchInput] = useState("");
+
+  console.log("NavBar render searchInput:", searchInput);
+
+  const searchLinkQuery = searchInput !== "" ? { q: searchInput } : {};
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <Link href="/" className="btn btn-ghost text-xl">
+          Spotify
+        </Link>
+      </div>
+      <div className="flex gap-2">
+        <input
+          type="text"
+          placeholder="Search"
+          className="input input-bordered w-24 md:w-auto"
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        />
+        <Link
+          href={{
+            pathname: "/search",
+            query: searchLinkQuery,
+          }}
+          className="btn btn-ghost text-xl"
+        >
+          Search
+        </Link>
+      </div>
+    </div>
+  );
+}
