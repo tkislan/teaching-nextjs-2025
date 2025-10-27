08a8d5e2868a9d0b2bbe1468b737ea77e11abeb9
Marek Tucani <marektucani@MacBook-Pro-22.local>
2025-10-28 00:19:36 +0100

    Add cypress

---
+++ cypress/e2e/interacting_with_ui.cy.ts
@@ -0,0 +1,9 @@
+describe('Album Catalog - Interactions', () => {
+    it('looks for songs when searching via search bar', () => {});
+
+    it('navigates to the first album detail', () => {});
+
+    it('navigates to home page after clicking on Spotify logo', () => {});
+
+    // add at least 3 more tests here
+});
