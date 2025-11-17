71b5eec927c0c8be435a577ffc6fb9048696796b
Marek Tučáni <marek.tucani@gmail.com>
2025-11-17 23:47:15 +0100

    Delete cypress/e2e/interacting_with_ui.cy.ts

--- cypress/e2e/interacting_with_ui.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('Album Catalog - Interactions', () => {
-    it('looks for songs when searching via search bar', () => {});
-
-    it('navigates to the first album detail', () => {});
-
-    it('navigates to home page after clicking on Spotify logo', () => {});
-
-    // add at least 3 more tests here
-});
