0067a00adcec61ff0e67706d8b13431723e9f6ca
tomas <tomas@kislan.sk>
2025-12-16 16:26:17 +0000

    Add create and update playlist forms and server actions

--- src/actions/playlists.ts
+++ src/actions/playlists.ts
@@ -2,16 +2,80 @@
 
 import { getDb } from "@/lib/db";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export async function createPlaylist(formData: FormData) {
+  const playlistName = formData.get("playlistName");
+
+  if (playlistName == null) {
+    throw new Error("Playlist name missing");
+  }
+
+  const playlistNameStr = playlistName.toString();
+
+  if (playlistNameStr === "") {
+    throw new Error("Playlist name cannot be empty");
+  }
+
+  const db = getDb();
+
+  const newPlaylist = await db
+    .insertInto("playlists")
+    .values({
+      name: playlistNameStr,
+      user_id: 1,
+    })
+    .returningAll()
+    .executeTakeFirstOrThrow();
+
+  redirect(`/playlist/${newPlaylist.id}`);
+}
+
+export async function updatePlaylist(formData: FormData) {
+  const playlistName = formData.get("playlistName");
+
+  if (playlistName == null) {
+    throw new Error("Playlist name missing");
+  }
+
+  const playlistNameStr = playlistName.toString();
+
+  if (playlistNameStr === "") {
+    throw new Error("Playlist name cannot be empty");
+  }
+
+  const playlistId = formData.get("playlistId");
+
+  if (playlistId == null) {
+    throw new Error("Playlist id missing");
+  }
+
+  const playlistIdInt = parseInt(playlistId.toString());
+
+  if (isNaN(playlistIdInt)) {
+    throw new Error("Invalid playlist id");
+  }
+
+  const db = getDb();
+
+  await db.updateTable("playlists").set({ name: playlistNameStr }).where("id", "=", playlistIdInt).execute();
+
+  revalidatePath(`/playlist/${playlistIdInt}`);
+  redirect(`/playlist/${playlistIdInt}`);
+}
 
 export async function addSongToPlaylist(playlistId: number, songId: number) {
   const db = getDb();
 
-  await db.insertInto('playlists_songs').values({
-    playlist_id: playlistId,
-    song_id: songId
-  }).execute()
+  await db
+    .insertInto("playlists_songs")
+    .values({
+      playlist_id: playlistId,
+      song_id: songId,
+    })
+    .execute();
 
-  revalidatePath('/')
+  revalidatePath("/");
 }
 
 export async function removeSongFromPlaylist(
@@ -22,7 +86,12 @@ export async function removeSongFromPlaylist(
   console.log(`Removing song ${songId} from playlist ${playlistId}`);
   const db = getDb();
 
-  await db.deleteFrom('playlists_songs').where('id', '=', id).where('playlist_id', '=', playlistId).where('song_id', '=', songId).execute()
+  await db
+    .deleteFrom("playlists_songs")
+    .where("id", "=", id)
+    .where("playlist_id", "=", playlistId)
+    .where("song_id", "=", songId)
+    .execute();
 
-  revalidatePath('/')
+  revalidatePath("/");
 }
--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -14,6 +14,9 @@ export default async function PlaylistsPage() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <p className="text-2xl font-bold">Playlists</p>
+        <Link className="btn" href="/playlists/new">
+          Create Playlist
+        </Link>
         <ul>
           {playlists.map((playlist) => (
             <li className="list-disc" key={playlist.id}>
